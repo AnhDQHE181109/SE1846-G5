72b6af71ca4e3a7142736c346fd4ff78f17e46ec
ASUS <lap1234.lpo@gmail.com>
2024-05-23 01:33:40 +0700

    Finishing up the backend of updating
    Initial "working" backend of updating the user's details

--- src/java/controller/UserDetailsServlet.java
+++ src/java/controller/UserDetailsServlet.java
@@ -4,12 +4,20 @@
  */
 package controller;
 
+import DAO.UserDetailsDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import model.*;
 
 /**
@@ -17,6 +25,20 @@ import model.*;
  * @author ASUS
  */
 public class UserDetailsServlet extends HttpServlet {
+
+    /*
+    For debugging purposes
+     */
+    public static void main(String[] args) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = null;
+        try {
+            date = formatter.parse("2024-05-27");
+        } catch (ParseException ex) {
+            System.out.println(ex);
+        }
+        System.out.println(date);
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -58,12 +80,12 @@ public class UserDetailsServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
+
         Account account = (Account) request.getSession().getAttribute("account");
         if (account != null) {
             request.setAttribute("account", account);
             System.out.println(account);
-            
+
             request.getRequestDispatcher("account_details.jsp").forward(request, response);
         } else {
             out.println("<h1>Invalid data!");
@@ -82,9 +104,89 @@ public class UserDetailsServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Account account = (Account) request.getSession().getAttribute("account");
+
+        String inputUsername = request.getParameter("inputUsername").trim();
+        String inputFirstName = request.getParameter("inputFirstName").trim();
+        String inputLastName = request.getParameter("inputLastName").trim();
+        String inputEmailAddress = request.getParameter("inputEmailAddress").trim();
+        String inputPhone = request.getParameter("inputPhone").trim();
+        String inputBirthday = request.getParameter("inputBirthday").trim();
+
+        String button = request.getParameter("saveChanges").trim();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = null;
+        try {
+            date = formatter.parse(inputBirthday);
+        } catch (ParseException ex) {
+            System.out.println(ex);
+        }
+        Account tempAccount = new Account(inputUsername, inputFirstName, inputLastName,
+                inputPhone, inputEmailAddress, new java.sql.Date(date.getTime()));
+
+        UserDetailsDAO userDetailsOps = new UserDetailsDAO();
+
+        if (account != null) {
+            if (button != null) {
+                if (inputUsername != null && inputFirstName != null && inputLastName != null
+                        && inputEmailAddress != null && inputPhone != null && inputBirthday != null) {
+                    if (userDetailsOps.isDuplicateUsername(inputUsername)) {
+                        out.print("<script>alert('This username is unavailable!')</script>");
+                        request.setAttribute("account", tempAccount);
+                        request.getRequestDispatcher("account_details.jsp").include(request, response);
+                        return;
+                    }
+
+                    if (userDetailsOps.getAccountByUsernameForComparison(inputUsername).equals(tempAccount)) {
+                        out.print("<script>alert('Nothing was changed!')</script>");
+                        request.setAttribute("account", tempAccount);
+                        request.getRequestDispatcher("account_details.jsp").include(request, response);
+                    } else {
+                        account.setFirstname(inputFirstName);
+                        account.setLastname(inputLastName);
+                        account.setEmail(inputEmailAddress);
+                        account.setPhoneNumber(inputPhone);
+
+                        account.setBirthDate(new java.sql.Date(date.getTime()));
+
+                        userDetailsOps.updateAccountInfo(account, inputUsername);
+
+                        request.setAttribute("account", account);
+
+                        out.print("<script>alert('Successfully updated your info!')</script>");
+                        request.getRequestDispatcher("account_details.jsp").include(request, response);
+                        //request.getRequestDispatcher("account_details.jsp").forward(request, response);
+                    }
+
+                } else {
+                    out.print("<script>alert('None of the fields can be empty!')</script>");
+                    request.setAttribute("account", tempAccount);
+                    request.getRequestDispatcher("account_details.jsp").include(request, response);
+                    return;
+                }
+
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Action forbidden");
+        }
     }
 
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        PrintWriter out = response.getWriter();
+//
+//        String inputBirthday = request.getParameter("inputBirthday").trim();
+//        System.out.println(inputBirthday);
+//        
+//        out.print("<script>alert('aaaa')</script>");
+//        request.getRequestDispatcher("account_details.jsp").include(request, response);
+//    }
     /**
      * Returns a short description of the servlet.
      *
